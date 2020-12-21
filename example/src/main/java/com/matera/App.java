6f750d3ea6c36cc12b2c0872ea17c08a7d275efa
izumizawa <izumizawa4@gmail.com>
2020-12-21 16:27:37 -0300

    first commit

---
+++ example/src/main/java/com/matera/App.java
@@ -0,0 +1,14 @@
+package com.matera;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //String argumento = args[0];
+        System.out.println( "Hello World! Esse é o primeiro exemplo para testar Maven! :)" /*"\n O argumento inserido primeiro é " + argumento*/);
+    }
+}
---
+++ example/src/test/java/com/matera/AppTest.java
@@ -0,0 +1,25 @@
+package com.matera;
+
+import static org.junit.Assert.assertTrue;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest
+{
+    //private String s;
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+
+        assertTrue( true );
+    }
+}
